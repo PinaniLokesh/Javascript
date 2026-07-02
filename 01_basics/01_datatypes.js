cc4baa93386a97b730e67373c5c47b4f462f44ff
PinaniLokesh <40831194+PinaniLokesh@users.noreply.github.com>
2026-07-02 09:51:13 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; //treat all code as newer version of Javacript
+
+// Primitive Datatypes
+
+let name="Lokesh" //String Datatype : Used with dobule or single quotes
+
+let age=18 // Number Datatype : Range is 2^53
+
+let Isloggedin=false // Boolean Datatype : For True or false statements 
+
+// Big Int datatype : Used for large nu8mbers and big formulas
+
+let temp=null//  Null is a datype as well as standalone value : It respresents empty value
+// Undefined datatype: When value is not defined 
+// Symbole : unique
+
+console.log(typeof temp);
