7b0850da214b87900dfc916850f1cf491837965a
PinaniLokesh <40831194+PinaniLokesh@users.noreply.github.com>
2026-07-02 08:35:36 +0000

    variables added

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountid=1234
+let accountemail="lokesh@google.com"
+var password="12345"
+
+accountcity="Jaipur"
+let accountstate
+
+// accountid=2
+
+console.log(accountid)
+console.table([accountid,accountemail,password,accountcity,accountstate])
+accountemail="abc@google.com"
+password="7894"
+accountcity="Delhi"
+
+console.table([accountid,accountemail,password,accountcity,accountstate])
