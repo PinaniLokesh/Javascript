70e506fad500595d30dbf33d5dd8d9926529e844
PinaniLokesh <40831194+PinaniLokesh@users.noreply.github.com>
2026-07-11 14:00:13 +0000

    Destructure

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -27,8 +27,23 @@ const obj2 = {3: "c", 4: "d"}
 const obj3 = { ...obj1, ...obj2} // Spread operator
 //  console.log(obj3);
 
-console.log(tinderuser);
-console.log(Object.keys(tinderuser));
-console.log(Object.values(tinderuser));
-console.log(Object.entries(tinderuser));
-console.log(tinderuser.hasOwnProperty('isloggedin'));
+// console.log(tinderuser);
+// console.log(Object.keys(tinderuser));
+// console.log(Object.values(tinderuser));
+// console.log(Object.entries(tinderuser));
+// console.log(tinderuser.hasOwnProperty('isloggedin'));
+
+// Destructuring Objects
+
+const course = {
+    coursename: "JS",
+    price: 555,
+    instructor:"ABC"
+}
+
+// console.log(course.instructor) // Prints Instructor value
+const {instructor : ins} = course  // stores instructor value in Ins
+
+// console.log(instructor);  
+console.log(ins);
+
