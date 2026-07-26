1ec690420b28c1c4ab4be72b8c4527482a548aa2
PinaniLokesh <40831194+PinaniLokesh@users.noreply.github.com>
2026-07-26 10:30:10 +0000

    while

---
+++ 05_iterations/two.js
@@ -0,0 +1,26 @@
+// While loop
+
+// let index = 0
+// while (index <= 10) {
+//     console.log(`value is ${index}`);
+//     index = index + 2
+//     // index + 1   // only returns incremented value by 1 to index but does not assign the value so th index value becomes 0 only and loop runs infinitly.
+// }
+
+// let myArray = ["Super", "Iron", "Spider"]
+// let arr = 0
+
+// while (arr < myArray.length) {
+//     console.log(`value is ${myArray[arr]}`);
+//     arr = arr + 1
+// }
+
+// do whiel loop
+
+let score = 0
+
+do {
+    console.log(`score is ${score}`);
+    score++
+    
+} while (score <= 10);
